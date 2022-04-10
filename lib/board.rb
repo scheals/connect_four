0fa4177d3e2c5ba7edccef7c1503e533eed34394
Scheals <piecoo@gmail.com>
2022-04-10 08:42:20 +0200

    Introduce user-friendly board representation

--- lib/board.rb
+++ lib/board.rb
@@ -5,4 +5,44 @@ class Gameboard
   def initialize
     @board = Array.new(6) { Array.new(7) { "\u26F6" } }
   end
+
+  # rubocop: disable Metrics/AbcSize
+  def show
+    puts <<-HEREDOC
+
+    || #{sixth_row[0]} || #{sixth_row[1]} || #{sixth_row[2]} || #{sixth_row[3]} || #{sixth_row[4]} || #{sixth_row[5]} || #{sixth_row[6]} ||
+    || #{fifth_row[0]} || #{fifth_row[1]} || #{fifth_row[2]} || #{fifth_row[3]} || #{fifth_row[4]} || #{fifth_row[5]} || #{fifth_row[6]} ||
+    || #{fourth_row[0]} || #{fourth_row[1]} || #{fourth_row[2]} || #{fourth_row[3]} || #{fourth_row[4]} || #{fourth_row[5]} || #{fourth_row[6]} ||
+    || #{third_row[0]} || #{third_row[1]} || #{third_row[2]} || #{third_row[3]} || #{third_row[4]} || #{third_row[5]} || #{third_row[6]} ||
+    || #{second_row[0]} || #{second_row[1]} || #{second_row[2]} || #{second_row[3]} || #{second_row[4]} || #{second_row[5]} || #{second_row[6]} ||
+    || #{first_row[0]} || #{first_row[1]} || #{first_row[2]} || #{first_row[3]} || #{first_row[4]} || #{first_row[5]} || #{first_row[6]} ||
+    \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C
+
+    HEREDOC
+  end
+  # rubocop: enable Metrics/AbcSize
+
+  def first_row
+    @board[5]
+  end
+
+  def second_row
+    @board[4]
+  end
+
+  def third_row
+    @board[3]
+  end
+
+  def fourth_row
+    @board[2]
+  end
+
+  def fifth_row
+    @board[1]
+  end
+
+  def sixth_row
+    @board[0]
+  end
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,20 +1,45 @@
 # frozen_string_literal: true
 
-# rubocop: disable Layout/LineLength
+# rubocop: disable Layout/LineLength, Metrics/BlockLength
 
 require_relative '../lib/board'
 
 # is a two-player connection board game, in which the players choose a color and
-# then take turns dropping colored tokens into a seven-column, six-row vertically suspended grid.
+# then take turns dropping colored tokens into a seven-columnstarter_board., six-row vertically suspended grid.
 # The pieces fall straight down, occupying the lowest available space within the column.
 # The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own tokens.
+
 describe Gameboard do
-  describe '#initialize' do
+  describe 'initialize' do
     subject(:new_board) { described_class.new }
     it 'stores a 2D array representation of the board in @board' do
       board_array = Array.new(6) { Array.new(7) { "\u26F6" } }
       expect(new_board.instance_variable_get(:@board)).to eq(board_array)
     end
   end
+  describe 'show' do
+    context 'when a new board is created' do
+      subject(:starter_board) { described_class.new }
+      it 'outputs a legible representation with no inputs' do
+        representiation = "\n    " \
+        "|| \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 ||" \
+          "\n    " \
+          "|| \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 ||" \
+          "\n    " \
+          "|| \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 ||" \
+          "\n    " \
+          "|| \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 ||" \
+          "\n    " \
+          "|| \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 ||" \
+          "\n    " \
+          "|| \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 || \u26F6 ||" \
+          "\n    " \
+          "\u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C \u268C" \
+          "\n\n"
+        expect(starter_board).to receive(:puts).with(representiation)
+        starter_board.show
+      end
+    end
+  end
 end
-# rubocop: enable Layout/LineLength
+# rubocop: enable Layout/LineLength, Metrics/BlockLength
