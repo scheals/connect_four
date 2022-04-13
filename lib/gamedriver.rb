4084ed1e6a459979d3f72f2db228c8fc0c29bed9
Scheals <piecoo@gmail.com>
2022-04-13 11:52:27 +0200

    Add instructions

--- lib/gamedriver.rb
+++ lib/gamedriver.rb
@@ -7,6 +7,7 @@ require_relative 'player'
 # This class handles running the game.
 class GameDriver
   def self.setup
+    puts instructions
     puts "What's the name of the first player?"
     first_name = gets.chomp
     puts "What's the name of the second player?"
@@ -21,5 +22,35 @@ class GameDriver
 
   def self.create_board
     Board.new
+  end
+
+  def self.instructions
+    puts <<-HEREDOC
+    Welcome to Connect Four!
+    This is a two player game where the objective is to have four tokens of your colour
+    in one of these arrangements:
+    - a row, e.g.:
+
+    \u26AB \u26AB \u26AB \u26AB
+
+    - a column, e.g.:
+
+    \u26AA
+    \u26AA
+    \u26AA
+    \u26AA
+
+    - a diagonal, e.g.:
+
+    \u26AB
+    \u26AB
+    \u26AB
+    \u26AB
+
+    You achieve this by dropping tokens from the top of the column to the first free space on its way down.
+    If all spaces are occupied but no one has won it is a tie.
+    Good luck and enjoy!
+
+    HEREDOC
   end
 end
